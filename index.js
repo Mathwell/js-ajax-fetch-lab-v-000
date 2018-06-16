f0d762b66d35630cffaefd4e6d6bf1555328906c
Olena Ageyeva <eageeva@hotmail.com>
2018-06-16 18:02:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,18 @@ function forkRepo() {
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  return '128b5610a5677c664cfa4844674e85a0bad04c86'
 }
+
+const token = getToken()
+const postData = {
+  body: 'Great stuff'
+};
+
+fetch('https://api.github.com/repos/:your_ghname/:your_repo/commits/:sha/comments', {
+  method: 'POST',
+  body: JSON.stringify(postData),
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => console.log(res));
